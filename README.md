b16469f109fa0e424b79a42f6537deae5c4b9fdf
Amandine <amandinedelavoie@live.fr>
2021-02-09 15:39:10 +0100

    remove setCounter error

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Deliveroo-react
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d324a3fe-11e5-4b21-b051-3621dd533afa/deploy-status)](https://app.netlify.com/sites/deliveroo-front-react-amande/deploys)
+
 ![screen](/src/assets/img/screen.gif)
--- src/components/PanierContent.js
+++ src/components/PanierContent.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 const PanierContent = ({ product }) => {
   // initialisation du state counter
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
 
   return (
     // si des produits sont présents dans le panier, je les affiches
     <div className="PanierContent" id={1}>
       <div className="counter">
         <span>+</span>
-        <span>{counter}</span>
+        <span>0</span>
         <span>-</span>
       </div>
       <div className="name-panier">{product.title}</div>
